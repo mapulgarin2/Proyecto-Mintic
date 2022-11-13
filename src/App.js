51723814e46b22412008be0d5fb0f57be0f2e6b1
unknown <mapulgarin2@gmail.com>
2022-11-13 13:52:50 -0500

    Agregamos boostrap al proyecto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Hola Tripulantes</h1>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
           className="App-link"
