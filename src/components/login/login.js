d3450470348e2573007f1b2d93cead352ec9f779
unknown <mapulgarin2@gmail.com>
2022-11-14 15:16:14 -0500

    Se crea login.css y se muestan el usuario y pass en ventana emergente

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container, Form, Button, Row, Col } from 'react-bootstrap'
+import "./login.css";
 
 export default class login extends React.Component {
   constructor(props) {
@@ -11,12 +12,12 @@ export default class login extends React.Component {
   }
 
   iniciarSesion(){
-    alert('Boton de inicio de sesion')
+    alert(`usuario: ${this.state.usuario} - password: ${this.state.pass}`);
   }
-  
+
   render() {
     return (
-      <Container id="login-container" style={{ marginTop: 200 }}>
+      <Container id="login-container" >
         <Row>
           <Col>
             <Row>
@@ -26,20 +27,20 @@ export default class login extends React.Component {
               <Col sm ="12" xs= "12" md = {{span : 4,offset :4}} lg = {{span : 4,offset :4}} xl = {{span : 4,offset :4}}>
                 <Form>
                   <Form.Group>
-                    <Form.Label style={{ float: 'left' }}>Usuario</Form.Label>
+                    <Form.Label >Usuario</Form.Label>
                     <Form.Control onChange={(e)=> this.setState({ usuario: e.target.value})} />
                      {/* {this.state.usuario} */}
                   </Form.Group>
 
                   <Form.Group>
-                    <Form.Label style={{ float: 'left' }}>Contraseña</Form.Label>
+                    <Form.Label >Contraseña</Form.Label>
                     <Form.Control type="password" onChange = {(e) => this.setState({ pass: e.target.value})} />
                      {/* {this.state.pass} */}
                   </Form.Group>
 
                   <Button
                     variant="primary"
-                    style={{ marginTop: 20, width: '100%' }}
+                    
                     onClick= {() =>{
                       this.iniciarSesion();
                     }}
