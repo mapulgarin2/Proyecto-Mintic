81888c3bf54d7292a5c6a751f72b1bbc757ba418
unknown <mapulgarin2@gmail.com>
2022-11-14 13:16:16 -0500

    Se modifica front de inicio de sesion

---
+++ src/components/login/login.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Container, Form, Button, Row, Col } from 'react-bootstrap'
+
+export default class login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usuario: '',
+      pass: '',
+    };
+  }
+  render() {
+    return (
+      <Container id="login-container" style={{ marginTop: 200 }}>
+        <Row>
+          <Col>
+            <Row>
+              <h2>Iniciar Sesion</h2>
+            </Row>
+            <Row>
+              <Col sm ="12" xs= "12" md = {{span : 4,offset :4}} lg = {{span : 4,offset :4}} xl = {{span : 4,offset :4}}>
+                <Form>
+                  <Form.Group>
+                    <Form.Label style={{ float: 'left' }}>Usuario</Form.Label>
+                    <Form.Control onChange={(e)=> this.setState({ usuario: e.target.value})} />
+                     {/* {this.state.usuario} */}
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label style={{ float: 'left' }}>Contraseña</Form.Label>
+                    <Form.Control type="password" onChange = {(e) => this.setState({ pass: e.target.value})} />
+                     {/* {this.state.pass} */}
+                  </Form.Group>
+
+                  <Button
+                    variant="primary"
+                    style={{ marginTop: 20, width: '100%' }}
+                  >
+                    Iniciar Sesion
+                  </Button>
+                </Form>
+              </Col>
+              
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
